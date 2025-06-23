b00388fdb99ca2512da98fc78bc5c5dcbd341f23
Oxana Bobrysheva <bobrysheva_oxana@mail.ru>
2025-06-23 14:54:25 +0300

    changes made

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from . import views
 from catalog import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
-    path('home/', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts'),
+
+    path('', views.home, name='home'),
+    path('', views.contacts, name='contacts'),
 ]
