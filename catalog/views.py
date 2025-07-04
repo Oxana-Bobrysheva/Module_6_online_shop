061860fa5aed3ad35573e7f40e30a115ebe74a26
Oxana Bobrysheva <bobrysheva_oxana@mail.ru>
2025-07-04 19:09:35 +0300

    tasks 1 and 2 were made (product_detail.html was added and controlleres were added

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 
 def home(request):
-    return render(request, "catalog/home.html")
+    products = Product.objects.all()
+    return render(request, "catalog/home.html", {"products": products})
 
 
 def contacts(request):
@@ -14,3 +16,10 @@ def contacts(request):
         print(f"Новое сообщение от {name}, телефон: {phone}. Текст: {message}")
         success = True
     return render(request, "catalog/contacts.html", {"success": success})
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        'product': product,
+    }
+    return render(request, 'catalog/product_detail.html', context)
