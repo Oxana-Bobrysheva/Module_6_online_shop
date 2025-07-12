eeac9c8f63dee21d4e39ca2702987277b693cfea
Oxana Bobrysheva <bobrysheva_oxana@mail.ru>
2025-07-12 15:05:49 +0300

    new app was created and a new model and form

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Заголовок")
+    text = models.TextField(verbose_name="Содержимое статьи")
+    image = models.ImageField(
+        upload_to="images/",
+        blank=True, null=True,
+        verbose_name="Изображение")
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Дата создания статьи"
+    )
+    is_published = models.BooleanField(verbose_name="Опубликовано", default=False)
+    views_count = models.PositiveIntegerField(verbose_name="Количество просмотров", default=0)
+
+    def __str__(self):
+        return self.title
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, TemplateView, DetailView, CreateView
 
