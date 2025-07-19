aa89142adeafa0d48f7f839df3f2da8057bed771
Oxana Bobrysheva <bobrysheva_oxana@mail.ru>
2025-07-19 17:43:42 +0300

    Image validation was added

--- catalog/forms.py
+++ catalog/forms.py
@@ -21,6 +21,8 @@ class ProductForm(forms.ModelForm):
             "purchase_price",
         ]
         exclude = ["created_at", "updated_at"]
+
+
     def __init__(self, *args, **kwargs):
         super(ProductForm, self).__init__(*args, **kwargs)
         self.fields['product_name'].widget.attrs.update({'class': 'form-control',
@@ -58,3 +60,21 @@ class ProductForm(forms.ModelForm):
         if price <= 0:
             raise ValidationError("Цена продукта не может быть отрицательной или равной нулю!")
         return price
+
+
+    def clean_product_image(self):
+        image = self.cleaned_data.get('product_image')
+
+        if image:
+            # Проверяем формат
+            valid_mime_types = ['image/jpeg', 'image/png']
+            file_mime_type = image.content_type
+            if file_mime_type not in valid_mime_types:
+                raise ValidationError('Поддерживаются только форматы JPEG и PNG.')
+
+            # Проверяем размер
+            max_size = 5 * 1024 * 1024  # 5 МБ
+            if image.size > max_size:
+                raise ValidationError('Размер файла не должен превышать 5 МБ.')
+
+        return image
