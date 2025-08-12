5902b095f18465da1cdc8cfcd7b7197f95692340
Oxana Bobrysheva <bobrysheva_oxana@mail.ru>
2025-08-12 14:11:47 +0300

    permissions for delete and moderators

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,10 @@
-from itertools import product
-
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
+from django.core.exceptions import PermissionDenied
 from django.urls import reverse_lazy
 from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
                                   TemplateView, UpdateView)
 
-from .forms import ProductForm
+from .forms import ProductForm, ProductModeratorForm
 from .models import Product
 
 
@@ -32,17 +31,23 @@ class ProductDetailView(LoginRequiredMixin, DetailView):
     model = Product
     template_name = "catalog/product_detail.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product = self.object
+        is_moderator = self.request.user.groups.filter(name='Moderators of products').exists()
+        context['is_moderator'] = is_moderator
+        return context
 
 class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
     form_class = ProductForm
     template_name = "catalog/product_form.html"
     success_url = reverse_lazy("catalog:home")
+    permission_required = 'catalog.add_product'
 
     def form_valid(self, form):
-        product = form.save()
-        user = self.request.user
-        product.owner = user
+        product = form.save(commit=False)
+        product.owner = self.request.user
         product.save()
         return super().form_valid(form)
 
@@ -55,9 +60,19 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
 
     def get_form_class(self):
         user = self.request.user
-        #if user ==
+        if user == self.object.owner:
+            return ProductForm
+        if user.has_perm("catalog.can_unpublish_product"):
+            return ProductModeratorForm
+        raise PermissionDenied
 
-class ProductDeleteView(LoginRequiredMixin, DeleteView):
+class ProductDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Product
     template_name = "catalog/product_confirm_delete.html"
     success_url = reverse_lazy("catalog:home")
+
+    def test_func(self):
+        product = self.get_object()
+        if product.owner == self.request.user:
+            return True
+        return self.request.user.groups.filter(name="Moderator of products")
